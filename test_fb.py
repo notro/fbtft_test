5096c63020b1c80666b973086716645516cd4f16
notro <notro@tronnes.org>
2013-02-24 20:08:38 +0100

    cleanup

--- test_fb.py
+++ test_fb.py
@@ -43,20 +43,44 @@ def pause(secs):
 	termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
 	fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
 
+def msg(fb, s, c, max_size):
+	width = fb.str_width(s)
+	for i in range(max_size+1, 1, -1):
+		if fb.xres > i*width:
+			fb.putstr(-1, -1, s, c, i)
+			return True
+	return False
+
+
+def show_name(fb, c):
+	fb.fill(0)
+	if not msg(fb, fb.name, c, 3):
+		msg(fb, 'Test', c, 3)
+	pause(2)
+
+
+def show_done(fb, c):
+	fb.fill(0)
+	msg(fb, 'Done', c, 3)
+	pause(2)
 
 
 def test_blank(fb, c):
+	print "  Blanking"
 	fb.fill(0)
-	fb.putstr(1, fb.yres/2, 'Blank', c, 1)
+	msg(fb, 'Blank  ', c, 2)
 	pause(1)
 	fb.blank(1)
-	pause(4)
+	pause(2)
 	fb.fill(0)
-	fb.putstr(1, fb.yres/2, 'Unblank', c, 1)
+	msg(fb, 'Unblank', c, 2)
+	sleep(0.5)
 	fb.blank(0)
+	pause(1)
 
 
-def test_outline(fb, c):
+def test_border(fb, c):
+	print "  Border"
 	fb.fill(0)
 	fb.draw.rect(c, Rect(0, 0, fb.xres-1, fb.yres-1), 1)
 
@@ -65,17 +89,23 @@ def test_outline(fb, c):
 
 	fb.draw.rect(c, Rect(fb.xres-6, 2, 4, 4), 0)
 	fb.draw.rect(c, Rect(fb.xres-6, fb.yres-6, 4, 4), 0)
+	pause(2)
 
 
 def test_raster(fb, c):
+	print "  Raster"
 	fb.fill(0)
 	for y in range(0, fb.yres, 2):
 		for x in range(0, fb.xres, 2):
 			fb.putpixel(x, y, c)
 			fb.putpixel(x+1, y+1, c)
+	pause(2)
 
 
 def test_rgb(fb):
+	if (fb.bits_per_pixel == 1):
+		return
+	print "  RGB"
 	fb.fill(0)
 	width = (fb.xres-1)/3
 	fb.draw.rect(fb.rgb(255,0,0), Rect(0, 0, width, fb.yres), 0)
@@ -86,45 +116,32 @@ def test_rgb(fb):
 
 	fb.draw.rect(fb.rgb(0,0,255), Rect(2*width, 0, width, fb.yres), 0)
 	fb.putstr(5+2*width, fb.yres/2, 'BLUE', 0, 1)
-	
+	pause(2)
+
+
 
 
 if __name__ == '__main__':
-	t = 5
 	fb = fb.Framebuffer('/dev/fb1')
 	print fb
 
-	t = 5
 	red = fb.rgb(255,0,0)
 	yellow = fb.rgb(255,255,0)
 	green = fb.rgb(0,255,0)
 
-	fb.fill(0)
+	show_name(fb, red)
 
-	# Show name
-	width = len(fb.name)*6+2
-	if fb.xres > width:
-		x = (fb.xres - width)/2
-		fb.putstr(x, fb.yres/2, fb.name, red, 1)
-	else:
-		str = 'Test'
-		width = len(str)*6+2
-		x = (fb.xres - width)/2
-		fb.putstr(x, fb.yres/2, str, red, 1)
-	pause(2)
+	print "Tests:"
 
-	if (fb.bits_per_pixel != 1):
-		test_rgb(fb)
-		pause(t)
+	test_border(fb, yellow)
 
-	test_outline(fb, yellow)
-	pause(t)
+	test_rgb(fb)
 
 	test_raster(fb, red)
-	pause(t)
 
 	test_blank(fb, red)
-	pause(t)
+
+	show_done(fb, red)
 
 	fb.fill(0)
 	fb.close()
