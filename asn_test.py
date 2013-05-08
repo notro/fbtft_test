4a59e0fb6252e5056c011a869a1b939b4ea30ca6
notro <notro@tronnes.org>
2013-05-08 18:39:36 +0000

    added test for itdb28fb

--- asn_test.py
+++ asn_test.py
@@ -5,133 +5,23 @@ Test code for protoboard with:
 * Nokia 3310
 '''
 
-import subprocess
-import time
-import os
-
-MPG_TEST = "/home/pi/test.mpg"
-
-def call(*args):
-	if subprocess.call(*args) != 0:
-		raise OSError
-
-def sudocall(cmd, *args):
-	if subprocess.call(["sudo"] + cmd, *args) != 0:
-		raise OSError
-
-def sudoecho(file, str):
-	os.system("sudo sh -c \"echo '%s' > %s\"" % (str, file))
-
-
-def apt_get_install(*args):
-	sudocall(["apt-get", "-y", "install"] + [item for item in args])
-
-def prerequisites():
-	dir = os.getcwd()
-	os.chdir(os.path.dirname(__file__))
-	os.chdir("..")
-
-	if not os.path.isdir("fbtest/"):
-		print("\nInstalling fbtest\n-----------------\n\n")
-		sudocall(["apt-get", "-y", "install", "libnetpbm10-dev"])
-		call(["git", "clone", "https://git.kernel.org/pub/scm/linux/kernel/git/geert/fbtest.git"])
-		text = open("fbtest/fb.c").read()
-		with open("fbtest/fb.c", "w") as f:
-			f.write(text.replace("#include <asm/page.h>", "/* #include <asm/page.h> */"))
-		os.chdir("fbtest")
-		call(["make"])
-
-	if not os.path.isfile(MPG_TEST):
-		print("\nInstalling mplayer\n------------------\n\n")
-		apt_get_install("mplayer")
-		call(["wget", "--directory-prefix=%s" % MPG_TEST, "http://fredrik.hubbe.net/plugger/test.mpg"])
-
-	os.chdir(dir)
-
-def ensure_spi():
-	sudocall(["modprobe", "spi_bcm2708"])
-
-def ensure_fbtft():
-	sudocall(["modprobe", "fbtft"])
-
-class FBTFTdevice:
-	def __init__(self, name, dev={}, drv={}):
-		self.name = name
-		cmd = ["modprobe", "--first-time", "fbtft_device", "name=%s" % name] + ["%s=%s" %(k,v) for k,v in dev.iteritems()]
-#		print " ".join(cmd)
-		sudocall(cmd)
-		sudocall(["modprobe", self.name] + ["%s=%s" %(k,v) for k,v in drv.iteritems()])
-
-	def __enter__(self):
-		return self
-
-	def __exit__(self, type, value, trace):
-		self.remove()
-
-	def remove(self):
-		sudocall(["rmmod", self.name])
-		sudocall(["rmmod", "fbtft_device"])
-
-def lsmod():
-#	if not "fbtft" in subprocess.check_output("lsmod"):
-	print subprocess.check_output("lsmod")
-
-def fbtest():
-	dir = os.getcwd()
-	os.chdir(os.path.dirname(__file__))
-	os.chdir("..")
-	sudocall(["./fbtest/fbtest", "-f", "/dev/fb1"])
-	os.chdir(dir)
-
-def mplayer_test(x, y):
-	sudocall(["mplayer", "-nolirc", "-vo", "fbdev2:/dev/fb1", "-endpos", "8", "-vf", "scale=%s:%s" % (x,y), MPG_TEST])
-
-def startx_test():
-	os.environ['FRAMEBUFFER'] = "/dev/fb1"
-	print "\n\n    To end the startx test, click Off button in lower right corner and press Alt-l (lowercase L) to logout"
-	call(["startx"])
-
-def console_test():
-	sudocall(["con2fbmap", "1", "1"])
-	time.sleep(2)
-	sudocall(["con2fbmap", "1", "0"])
-
-def bl_power_test():
-	file="/sys/class/backlight/%s/bl_power" % dev.name
-	if os.path.isfile(file):
-		time.sleep(1)
-		sudoecho(file, "1")
-		time.sleep(2)
-		sudoecho(file, "0")
-		time.sleep(1)
-
-def blank_test():
-	file="/sys/class/graphics/fb1/blank"
-	if os.path.isfile(file):
-		time.sleep(1)
-		sudoecho(file, "4")
-		time.sleep(2)
-		sudoecho(file, "0")
-		time.sleep(1)
+from fbtft import *
 
 prerequisites()
-#quit()
 
 ensure_spi()
 ensure_fbtft()
 
-
-
 with FBTFTdevice("adafruit22fb", dev={'gpios' : "reset:25,led:18"}) as dev:
 	fbtest()
 	mplayer_test(176, 220)
-	startx_test()
-	console_test()
-	bl_power_test()
+	bl_power_test(dev)
 	blank_test()
+	startx_test()
 
 # Rotate
 with FBTFTdevice("adafruit22fb", drv={'rotate':3}, dev={'gpios' : "reset:25,led:18"}) as dev:
+	console_test()
 	mplayer_test(220, 176)
 
 
@@ -139,10 +29,10 @@ with FBTFTdevice("sainsmart18fb", dev={'cs':1, 'gpios':"reset:23,dc:24"}) as dev
 	fbtest()
 	mplayer_test(128, 160)
 	startx_test()
-	console_test()
 
 # Rotate
 with FBTFTdevice("sainsmart18fb", drv={'rotate':1}, dev={'cs':1, 'gpios':"reset:23,dc:24"}) as dev:
+	console_test()
 	mplayer_test(160, 128)
 
 
---
+++ fbtft.py
@@ -0,0 +1,113 @@
+import subprocess
+import time
+import os
+
+MPG_TEST = "/home/pi/test.mpg"
+
+def call(*args):
+	if subprocess.call(*args) != 0:
+		raise OSError
+
+def sudocall(cmd, *args):
+	if subprocess.call(["sudo"] + cmd, *args) != 0:
+		raise OSError
+
+def sudoecho(file, str):
+	os.system("sudo sh -c \"echo '%s' > %s\"" % (str, file))
+
+
+def apt_get_install(*args):
+	sudocall(["apt-get", "-y", "install"] + [item for item in args])
+
+def prerequisites():
+	dir = os.getcwd()
+	os.chdir(os.path.dirname(__file__))
+	os.chdir("..")
+
+	if not os.path.isdir("fbtest/"):
+		print("\nInstalling fbtest\n-----------------\n\n")
+		sudocall(["apt-get", "-y", "install", "libnetpbm10-dev"])
+		call(["git", "clone", "https://git.kernel.org/pub/scm/linux/kernel/git/geert/fbtest.git"])
+		text = open("fbtest/fb.c").read()
+		with open("fbtest/fb.c", "w") as f:
+			f.write(text.replace("#include <asm/page.h>", "/* #include <asm/page.h> */"))
+		os.chdir("fbtest")
+		call(["make"])
+
+	if not os.path.isfile(MPG_TEST):
+		print("\nInstalling mplayer\n------------------\n\n")
+		apt_get_install("mplayer")
+		call(["wget", "--directory-prefix=%s" % MPG_TEST, "http://fredrik.hubbe.net/plugger/test.mpg"])
+
+	os.chdir(dir)
+
+def ensure_spi():
+	sudocall(["modprobe", "spi_bcm2708"])
+
+def ensure_no_spi():
+	sudocall(["modprobe", "-r", "spi_bcm2708"])
+
+def ensure_fbtft():
+	sudocall(["modprobe", "fbtft"])
+
+class FBTFTdevice:
+	def __init__(self, name, dev={}, drv={}, devname=""):
+		self.name = name
+		if not devname: devname = name
+		self.devname = devname
+		cmd = ["modprobe", "--first-time", "fbtft_device", "name=%s" % devname] + ["%s=%s" %(k,v) for k,v in dev.iteritems()]
+#		print " ".join(cmd)
+		sudocall(cmd)
+		sudocall(["modprobe", self.name] + ["%s=%s" %(k,v) for k,v in drv.iteritems()])
+
+	def __enter__(self):
+		return self
+
+	def __exit__(self, type, value, trace):
+		self.remove()
+
+	def remove(self):
+		sudocall(["rmmod", self.name])
+		sudocall(["rmmod", "fbtft_device"])
+
+def lsmod():
+#	if not "fbtft" in subprocess.check_output("lsmod"):
+	print subprocess.check_output("lsmod")
+
+def fbtest():
+	dir = os.getcwd()
+	os.chdir(os.path.dirname(__file__))
+	os.chdir("..")
+	sudocall(["./fbtest/fbtest", "-f", "/dev/fb1"])
+	os.chdir(dir)
+
+def mplayer_test(x, y):
+	sudocall(["mplayer", "-nolirc", "-vo", "fbdev2:/dev/fb1", "-endpos", "8", "-vf", "scale=%s:%s" % (x,y), MPG_TEST])
+
+def startx_test():
+	os.environ['FRAMEBUFFER'] = "/dev/fb1"
+	print "\n\n    To end the startx test, click Off button in lower right corner and press Alt-l (lowercase L) to logout"
+	call(["startx"])
+
+def console_test():
+	sudocall(["con2fbmap", "1", "1"])
+	time.sleep(2)
+	sudocall(["con2fbmap", "1", "0"])
+
+def bl_power_test(dev):
+	file="/sys/class/backlight/%s/bl_power" % dev.name
+	if os.path.isfile(file):
+		time.sleep(1)
+		sudoecho(file, "1")
+		time.sleep(2)
+		sudoecho(file, "0")
+		time.sleep(1)
+
+def blank_test():
+	file="/sys/class/graphics/fb1/blank"
+	if os.path.isfile(file):
+		time.sleep(1)
+		sudoecho(file, "4")
+		time.sleep(2)
+		sudoecho(file, "0")
+		time.sleep(1)
