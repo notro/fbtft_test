c91a31e7c1abe9277678eb0709fb4d7b3d8c9506
notro <notro@tronnes.org>
2013-08-18 16:42:29 +0000

    rotate is now an angle

--- asn_test.py
+++ asn_test.py
@@ -13,14 +13,14 @@ ensure_spi()
 ensure_fbtft()
 
 
-for rotate in [0,1,2,3]:
+for rotate in [0,90,180,270]:
 	with FBTFTdevice("adafruit22", dev={ 'rotate':rotate, 'gpios' : "reset:25,led:18" }, autoload=True) as dev:
 		if rotate == 0:
 			console_test()
 			fbtest()
 			bl_power_test(dev)
 			startx_test()
-		if rotate % 2:
+		if rotate % 180:
 			mplayer_test(220,176)
 		else:
 			mplayer_test(176, 220)
@@ -30,26 +30,26 @@ for rotate in [0]:
 	if rotate == 0:
 		flexfb_drv_args = { 'chip':'hx8340bn', 'buswidth':9 }
 		#flexfb_drv_args = { 'width':176, 'height':220, 'buswidth':9, 'init':"-1,0xC1,0xFF,0x83,0x40,-1,0x11,-2,150,-1,0xCA,0x70,0x00,0xD9,-1,0xB0,0x01,0x11,-1,0xC9,0x90,0x49,0x10,0x28,0x28,0x10,0x00,0x06,-2,20,-1,0xC2,0x60,0x71,0x01,0x0E,0x05,0x02,0x09,0x31,0x0A,-1,0xC3,0x67,0x30,0x61,0x17,0x48,0x07,0x05,0x33,-2,10,-1,0xB5,0x35,0x20,0x45,-1,0xB4,0x33,0x25,0x4C,-2,10,-1,0x3A,0x05,-1,0x29,-2,10,-3" }
-	elif rotate == 1:
+	elif rotate == 90:
 		pass
-	elif rotate == 2:
+	elif rotate == 180:
 		pass
-	elif rotate == 3:
+	elif rotate == 270:
 		pass
 	with FBTFTdevice("flexfb", dev={ 'rotate':rotate, 'gpios':"reset:25,led:18" }, drv=flexfb_drv_args) as dev:
-		if rotate in [0,2]:
+		if rotate in [0,180]:
 			mplayer_test(176, 220)
 		else:
 			mplayer_test(220, 176)
 
 
-for rotate in [0,1,2,3]:
+for rotate in [0,90,180,270]:
 	with FBTFTdevice("sainsmart18", dev={ 'rotate':rotate, 'cs':1, 'gpios':"reset:23,dc:24" }, autoload=True) as dev:
 		if rotate == 0:
 			console_test()
 			fbtest()
 			startx_test()
-		if rotate % 2:
+		if rotate % 180:
 			mplayer_test(160,128)
 		else:
 			mplayer_test(128, 160)
@@ -57,14 +57,14 @@ for rotate in [0,1,2,3]:
 for rotate in [0]:
 	if rotate == 0:
 		flexfb_drv_args = { 'chip':'st7735r' }
-	elif rotate == 1:
+	elif rotate == 90:
 		pass
-	elif rotate == 2:
+	elif rotate == 180:
 		pass
-	elif rotate == 3:
+	elif rotate == 270:
 		pass
 	with FBTFTdevice("flexfb", dev={ 'rotate':rotate, 'cs':1, 'gpios':"reset:23,dc:24" }, drv=flexfb_drv_args) as dev:
-		if rotate in [0,2]:
+		if rotate in [0,180]:
 			mplayer_test(128, 160)
 		else:
 			mplayer_test(160, 128)
--- itdb_spi_test.py
+++ itdb_spi_test.py
@@ -13,28 +13,28 @@ ensure_fbtft()
 
 os.environ['DISPLAY'] = ":0"
 
-for rotate in [0,1,2,3]:
+for rotate in [180,270,0,90]:
 	with FBTFTdevice("itdb28_spi", dev={'rotate':rotate, 'gpios':"reset:25,dc:24,led:18"}, autoload=True) as dev:
 		console_test()
 		ads7846args = { 'x_min':230, 'x_max':3900, 'y_min':200, 'y_max':3700, 'x_plate_ohms':80, 'pressure_max':255, 'gpio_pendown':17 }
-		if rotate % 2:
+		if rotate % 180:
 			ads7846args['swap_xy'] = 1
 		with ADS7846device(dev=ads7846args):
 			x = startx_test(wait=False)
 			time.sleep(3)
-			if rotate == 1:
+			if rotate == 270:
 				call(["xinput", "--set-prop", 'ADS7846 Touchscreen', 'Evdev Axis Inversion', '0', '1'])
-			if rotate == 2:
+			if rotate == 180:
 				call(["xinput", "--set-prop", 'ADS7846 Touchscreen', 'Evdev Axis Inversion', '1', '1'])
-			if rotate == 3:
+			if rotate == 90:
 				call(["xinput", "--set-prop", 'ADS7846 Touchscreen', 'Evdev Axis Inversion', '1', '0'])
 			while x.poll() == None:
 				time.sleep(0.5)
 
-		if rotate % 2:
+		if rotate % 180:
 			mplayer_test(320, 240)
 		else:
 			mplayer_test(240, 320)
-		if rotate == 0:
+		if rotate == 180:
 			fbtest()
 			bl_power_test(dev)
